dc5c5b96e03e5887ad2a4a8b334bfc75909d7419
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-16 01:00:35 +0530

    first commit

--- src/components/new-collection/collection-cards/collection-cards.js
+++ src/components/new-collection/collection-cards/collection-cards.js
@@ -1,3 +1,4 @@
+import DetailCard from "../../newdetailCard/detailCard";
 import "./collection-cards.css";
 import { NavLink } from "react-router";
 
@@ -18,6 +19,12 @@ const Cards = ({ image, title, sub_title, price , id }) => {
         <div className="card-sub">{sub_title}</div>
         <div className="card-price">${price}</div>
       </div>
+         <DetailCard 
+             id={id}
+              image={image}
+              title={title}
+              sub_title={sub_title}
+              price={price}/>
     </NavLink>
   );
 };
--- src/components/newdetailCard/detailCard.js
+++ src/components/newdetailCard/detailCard.js
@@ -1,14 +1,15 @@
 import "./detailCard.css";
+
 const DetailCard = ({ image, title, sub_title, price, id }) => {
     return (
         <div className="card-detail" id={id}>
             <div
                 className="card-image-detail"
-               
+                style={{ backgroundImage: `url(${image})` }}
             ></div>
             <div className="card-title-detail">{title}</div>
             <div className="card-sub-detail">{sub_title}</div>
-            <div className="card-price-detail">${price}</div>
+            <div className="card-price-detail">Price : ${price}</div>
         </div>
     );
 };
